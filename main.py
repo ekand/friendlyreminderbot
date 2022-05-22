ba028949212aee3fa7c2a20e01d23b47a351dd46
Erik Kristofer Anderson <erik.kristofer.anderson@gmail.com>
2022-05-22 12:12:40 -0500

    use yaml parser

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ from load_env import load_twitter_env # function for loading keys!
 from time import sleep
 import random
 import tweepy
+from yaml import load, dump
+try:
+    from yaml import CLoader as Loader
+except ImportError:
+    from yaml import Loader
 
 # Loading twitter credentials
 consumer_key, consumer_secret, access_token, access_token_secret = load_twitter_env()
@@ -13,21 +18,27 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 # Connect to the TWITTER API
-api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+# api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 def reminder():
-   while True:
-     with open('reminders.txt') as f: lines = f.readlines()
+    while True:
+        with open('reminders.txt') as f: reminder_data = load(f.read(), Loader=Loader)
      
-     # Select a random line from the reminders file.
-     lines = [line[1:] for line in lines if line[0] == "-"]
-     line = lines[random.randint(0, len(lines) - 1)]
-     remind = line.strip()
+        # Select a random line from the reminders file.
+        reminders = []     
+        for category_key in reminder_data.keys():
+            category_data = reminder_data[category_key]
+            for subcategory_key in category_data.keys():
+                for reminder_raw_text in category_data[subcategory_key]:
+                    reminders.append(reminder_raw_text.strip())
+
+        reminder = random.choice(reminders)
     
-     # Random reminder tweets 
-     api.update_status(status=remind)
-     # sleeps for 4 hours
-     sleep(14400)
+        # Random reminder tweets 
+        print(reminder)
+        #api.update_status(status=reminder)
+        # sleeps for 4 hours
+        sleep(14400)
 
 if __name__ == "__main__":
    reminder()
